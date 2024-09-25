5e7444e57e049279e366b52ba517fa7f6b38ab70
Yᴀsʜ <unknownsoumo@gmail.com>
2024-09-25 18:52:19 +0530

    good

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <TonConnectUIProvider :options="options">
+            <div class="flex flex-col items-center">
+                <TonConnectButton class="bg-red-700 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mb-4" />
+                <TheWelcomeVue />
+            </div>
+        </TonConnectUIProvider>
+    </div>
+</template>
+
+<script>
+import {
+    TonConnectUIProvider,
+    TonConnectButton,
+} from '@townsquarelabs/ui-vue';
+import TheWelcomeVue from './Node/Welcome.vue';
+
+export default {
+    components: {
+        TonConnectUIProvider,
+        TonConnectButton,
+        TheWelcomeVue
+    },
+    setup() {
+        const options = {
+            manifestUrl: "https://maybeyash.github.io/Fizzy/Fizzy-Ton.json",
+        };
+
+        return {
+            options,
+        }
+    }
+}
+</script>
