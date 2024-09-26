0e80aba58935216a14582becbd454eb8f6135057
Yᴀsʜ <unknownsoumo@gmail.com>
2024-09-26 08:56:31 +0530

    good

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- src/App.vue -->
+<template>
+  <div>
+    <ConnectWallet />
+  </div>
+</template>
+
+<script>
+import ConnectWallet from './components/ConnectWallet.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ConnectWallet,
+  },
+};
+</script>
